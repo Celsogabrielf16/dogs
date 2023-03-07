6f9469ac869dc9ecaeebd7d2cc045ec8d5ad6ddd
Celso Gabriel Prado <celsogabrielf@gmail.com>
2023-03-07 16:09:27 -0300

    header - Atualização do componente Header e sua estilização

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Header.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import { ReactComponent as Dogs } from "../Assets/dogs.svg";
 
 const Header = () => {
   return (
     <header>
-      <nav className='container'>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
+      <nav className="container">
+        <Link className="logo" to="/" aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        <Link className="login" to="/login">Login/Criar</Link>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
