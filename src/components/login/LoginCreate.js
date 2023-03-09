e0ed27c0ea5b828f4b607497148d06a6ba62618f
Celso Gabriel Prado <celsogabrielf@gmail.com>
2023-03-09 10:50:14 -0300

    integração do componente LoginCreate

--- src/components/login/LoginCreate.js
+++ src/components/login/LoginCreate.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import { USER_POST } from '../../api';
+import useForm from '../../Hooks/useForm';
+import UserContext from '../../UserContext';
+import Button from '../Form/Button';
+import Input from '../Form/Input';
 
 const LoginCreate = () => {
+  const username = useForm();
+  const email = useForm('email');
+  const password = useForm();
+
+  const { userLogin } = React.useContext(UserContext)
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const response = await fetch(url, options);
+    if (response.ok) userLogin(username.value, password.value)
+    console.log(response);
+  }
+
   return (
-    <div>LoginCreate</div>
+    <section className='animeLeft LoginCreateComponent'>
+      <h1 className='title'>Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
   )
 }
 
