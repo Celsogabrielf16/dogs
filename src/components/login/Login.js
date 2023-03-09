eeb09a6b63d62600a7908bba5d9bfeea276c0ea7
Celso Gabriel Prado <celsogabrielf@gmail.com>
2023-03-09 10:10:55 -0300

    estilização do componente Login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,26 +1,28 @@
-import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import UserContext from '../../UserContext'
-import './Login.css'
-import LoginCreate from './LoginCreate'
-import LoginForm from './LoginForm'
-import LoginPasswordLost from './LoginPasswordLost'
-import LoginPasswordReset from './LoginPasswordReset'
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import UserContext from "../../UserContext";
+import "./Login.css";
+import LoginCreate from "./LoginCreate";
+import LoginForm from "./LoginForm";
+import LoginPasswordLost from "./LoginPasswordLost";
+import LoginPasswordReset from "./LoginPasswordReset";
 
 const Login = () => {
-  const {login} = React.useContext(UserContext);
+  const { login } = React.useContext(UserContext);
 
-  if (login === true) return <Navigate to="/conta" />
+  if (login === true) return <Navigate to="/conta" />;
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={ <LoginForm />}/>
-        <Route path='criar' element={ <LoginCreate />}/>
-        <Route path='perdeu' element={ <LoginPasswordLost />}/>
-        <Route path='resetar' element={ <LoginPasswordReset />}/>
-      </Routes>
-    </div>
-  )
-}
+    <section className="LoginComponent">
+      <div className="forms">
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="perdeu" element={<LoginPasswordLost />} />
+          <Route path="resetar" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
+  );
+};
 
-export default Login
+export default Login;
