245219ed516ad5120fdd43e8249b96d6ebcb4f26
Celso Gabriel Prado <celsogabrielf@gmail.com>
2023-03-09 11:13:54 -0300

    criação do Custom Hook useFetch

--- src/components/login/LoginCreate.js
+++ src/components/login/LoginCreate.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import { USER_POST } from '../../api';
-import useForm from '../../Hooks/useForm';
-import UserContext from '../../UserContext';
-import Button from '../Form/Button';
-import Input from '../Form/Input';
+import React from "react";
+import { USER_POST } from "../../api";
+import useFetch from "../../Hooks/useFetch";
+import useForm from "../../Hooks/useForm";
+import UserContext from "../../UserContext";
+import Button from "../Form/Button";
+import Input from "../Form/Input";
+import Error from "../Helper/Error";
 
 const LoginCreate = () => {
   const username = useForm();
-  const email = useForm('email');
+  const email = useForm("email");
   const password = useForm();
 
-  const { userLogin } = React.useContext(UserContext)
+  const { userLogin } = React.useContext(UserContext);
+  const { loading, error, request } = useFetch();
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -19,22 +22,26 @@ const LoginCreate = () => {
       email: email.value,
       password: password.value,
     });
-    const response = await fetch(url, options);
-    if (response.ok) userLogin(username.value, password.value)
-    console.log(response);
+    const { response } = await request(url, options);
+    if (response.ok) userLogin(username.value, password.value);
   }
 
   return (
-    <section className='animeLeft LoginCreateComponent'>
-      <h1 className='title'>Cadastre-se</h1>
+    <section className="animeLeft LoginCreateComponent">
+      <h1 className="title">Cadastre-se</h1>
       <form onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Email" type="email" name="email" {...email} />
         <Input label="Senha" type="password" name="password" {...password} />
-        <Button>Cadastrar</Button>
+        {loading ? (
+          <Button disabled>Cadastrando</Button>
+        ) : (
+          <Button>Cadastrar</Button>
+        )}
+        <Error error={error}/>
       </form>
     </section>
-  )
-}
+  );
+};
 
-export default LoginCreate
+export default LoginCreate;
