99af480690f106946958382e43c5ba348a99ca05
Celso Gabriel Prado <celsogabrielf@gmail.com>
2023-03-13 13:25:46 -0300

    criação do componente UserProfile

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Home from "./components/Home";
 import Login from "./components/Login/Login";
 import Photo from "./components/Photo/Photo";
 import User from "./components/User/User";
+import UserProfile from "./components/User/UserProfile";
 import { UserStorage } from "./UserContext";
 
 const App = () => {
@@ -20,6 +21,7 @@ const App = () => {
             <Route path="/" element={<Home />} />
             <Route path="login/*" element={<Login />} />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
             <Route
               path="conta/*"
               element={
