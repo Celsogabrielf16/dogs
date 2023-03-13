659a33e170ac1c673ebf957dac9bff62dc15b431
Celso Gabriel Prado <celsogabrielf@gmail.com>
2023-03-13 15:01:59 -0300

    criação do componente Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { ReactComponent as Svg } from '../Assets/dogs-footer.svg'
 import './Footer.css'
 
 const Footer = () => {
   return (
-    <footer></footer>
+    <footer className='FooterComponent'>
+      <Svg/>
+      <p>Dogs. Todos os direitos reservados.</p>
+    </footer>
   )
 }
 
