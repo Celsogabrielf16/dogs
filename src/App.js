6d32e317935b5f974b2fb372694438a1ad2e8da8
Celso Gabriel Prado <celsogabrielf@gmail.com>
2023-03-13 13:30:38 -0300

    criação da página NotFound

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import ProtectedRoute from "./components/Helper/ProtectedRoute";
 import Home from "./components/Home";
 import Login from "./components/Login/Login";
+import NotFound from "./components/NotFound";
 import Photo from "./components/Photo/Photo";
 import User from "./components/User/User";
 import UserProfile from "./components/User/UserProfile";
@@ -30,6 +31,7 @@ const App = () => {
                 </ProtectedRoute>
               }
             />
+            <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
         </UserStorage>
